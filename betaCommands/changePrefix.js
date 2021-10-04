79117603a025e8c0fe9574143e7630c6bdfbf5e1
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2021-10-04 02:49:47 -0300

    second commit

--- betaCommands/changePrefix.js
+++ betaCommands/changePrefix.js
@@ -4,7 +4,7 @@ module.exports = {
     get howUse(){ return this.prefix[0] + " novoPrefixo"},
     prefix: ["prefix"], 
     func: (msg) => {
-        msg.server.prefix = "tete"
+        msg.server.prefix = "tete";
         msg.saveServer();
         msg.reply("prefixo é tete");
     }
